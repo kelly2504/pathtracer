a57f2066d78148e19dfa067f65f71414817179b6
Kelly <Kelly@DESKTOP-JE91G8G>
2025-11-07 22:08:13 +1100

    Add project files.

---
+++ pathtracer/color.h
@@ -0,0 +1,12 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "vec3.h"
+#include <iostream>
+#include <fstream>
+
+using color = vec3;
+
+void write_color(std::ostream& out, const color& pixel_color);
+
+#endif // !COLOR_H
