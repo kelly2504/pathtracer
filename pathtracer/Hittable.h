d0a777732b3e5c48f4d037c08e5c0b577f82b7a6
Kelly <kellywwk4@gmail.com>
2026-01-21 19:04:55 +1100

    Adding Surface normal  geometry

--- pathtracer/Hittable.h
+++ pathtracer/Hittable.h
@@ -10,6 +10,25 @@ class hit_record {
         point3 p;
         vec3 normal;
         double t;
+        bool front_face;
+
+        void set_face_normal(const ray& r , const vec3& outward_normal) {
+            //sets the hit record normal vector
+            //NOTE: the parameter `outward_normal` is assumed to have length
+            
+            //front_face = dot(r.direction(), outward_normal) < 0;
+            //normal = front_face ? outward_normal : -outward_normal;
+            //SURFACE-NORMAL GEOMETRY - front face tracking
+            if (dot(r.direction(), outward_normal) > 0) {
+                //ray is inside the sphere
+                normal = -outward_normal;
+                front_face = false;
+            } else {
+                //ray is outside the sphere
+                normal = outward_normal;
+                front_face = true;
+            }
+        }
 };
 
 class hittable {
